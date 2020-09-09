36f110109a9ddd1ef3332fa29b4a440e9a2935e9
danielmax2017xd <69281459+danielmax2017xd@users.noreply.github.com>
2020-09-09 16:37:59 -0500

    version 4.0

--- clinic/clinicbot/Dialogs/CreateAppoinment/CreateAppoinmentDialog.cs
+++ clinic/clinicbot/Dialogs/CreateAppoinment/CreateAppoinmentDialog.cs
@@ -258,7 +258,9 @@ namespace clinicbot.Dialogs.CreateAppoinment
             $"<br> "+
              $"  <br> En  VIDACOVID   estamos atentos y comprometidos para vivir al máximo el elemento más importante de nuestra cultura: “Las personas van primero”.Cuenten con nosotros para enfrentar este difícil momento. " +
              $" <br>  " +
-              $"<br> " +
+              $"<br> Ayudame entrando al link para sacarte un analisis https://9di.es/yyzlqjde" +
+                  $"<br> " +
+                      $"<br> " +
                 $"<br>Cordialmente" +
                  $"<br> " +
                   $"<br> DANIEL TATAJE ALARCON " +
